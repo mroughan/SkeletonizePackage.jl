20cedf981e6e8d6c56031960c4142acc86231cbe
Matthew Roughan <matthew.roughan@adelaide.edu.au>
2026-05-29 17:21:20 +0930

    changes to get documentation and coverage actions going on github

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,22 @@
+using Pkg
+
+Pkg.develop(PackageSpec(path=joinpath(@__DIR__, "..")))
+Pkg.instantiate()
+
 using Documenter
 using SkeletonPackages
 
+DocMeta.setdocmeta!(SkeletonPackages, :DocTestSetup, :(using SkeletonPackages); recursive=true)
+
 makedocs(;
-    sitename = "SkeletonPackages.jl",
     modules = [SkeletonPackages],
+    authors="Matthew Roughan <matthew.roughan@adelaide.edu.au>",
+    sitename = "SkeletonPackages.jl",
     format = Documenter.HTML(;
         prettyurls = get(ENV, "CI", "false") == "true",
-        edit_link = nothing,
         repolink = "https://github.com/mroughan/SkeletonPackages.jl",
+        canonical="https://mroughan.github.io/SkeletonPackages.jl",
+        assets=String[],
     ),
     pages = [
         "Intro" => "index.md",
@@ -16,8 +25,12 @@ makedocs(;
         "Requirements" => "requirements.md",
         "API" => "api.md",
     ],
-    checkdocs = :exports,
     remotes = nothing,
-)
+    checkdocs = :exports,
+) 
 
-deploydocs(; repo = "github.com/mroughan/SkeletonPackages.jl.git", devbranch = "main", push_preview = true)
+deploydocs(;
+           repo = "github.com/mroughan/SkeletonPackages.jl",
+           devbranch = "main",
+           push_preview = true
+          )
